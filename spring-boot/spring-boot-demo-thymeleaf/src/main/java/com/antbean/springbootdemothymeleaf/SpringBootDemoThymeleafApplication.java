ba71d26f43cfc3e6c8b792abab41460c81f370f9
lengyucc <1902328305@qq.com>
2017-05-15 09:45:45 +0800

    init
    Signed-off-by: lengyucc <1902328305@qq.com>

---
+++ spring-boot/spring-boot-demo-thymeleaf/src/main/java/com/antbean/springbootdemothymeleaf/SpringBootDemoThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.antbean.springbootdemothymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoThymeleafApplication.class, args);
+	}
+}
