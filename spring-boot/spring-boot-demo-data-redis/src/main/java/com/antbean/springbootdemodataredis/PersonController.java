8b604ca0d19d92d1eee5713a2d8e83ba8f3a4796
lengyucc <1902328305@qq.com>
2017-06-03 14:42:12 +0800

    test

---
+++ spring-boot/spring-boot-demo-data-redis/src/main/java/com/antbean/springbootdemodataredis/PersonController.java
@@ -0,0 +1,29 @@
+package com.antbean.springbootdemodataredis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+	@Autowired
+	PersonDao personDao;
+
+	@RequestMapping("/set")
+	public void set() {
+		Person person = new Person("1", "lmh", 22);
+		personDao.save(person);
+		personDao.stringRedisTemplateDemo();
+	}
+
+	@RequestMapping("/getStr")
+	public String getStr() {
+		return personDao.getString();
+	}
+
+	@RequestMapping("/getPerson")
+	public Person getPerson() {
+		return personDao.getPerson();
+	}
+
+}
