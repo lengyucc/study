b733447aaee36329bac50f014d4a70140ebce5fc
lengyucc <1902328305@qq.com>
2017-06-07 09:05:18 +0800

    test

--- spring-boot/spring-boot-demo-jms/readme.txt
+++ spring-boot/spring-boot-demo-jms/readme.txt
@@ -1,2 +1,3 @@
 activemq下载地址:http://mirror.bit.edu.cn/apache/activemq/5.14.5/apache-activemq-5.14.5-bin.zip
 activemq启动命令:bin/activemq start
+activemq管理账户:admin/admin
--- spring-boot/spring-boot-demo-jms/src/main/java/com/antbean/springbootdemojms/Receiver.java
+++ spring-boot/spring-boot-demo-jms/src/main/java/com/antbean/springbootdemojms/Receiver.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Receiver {
 
-	@JmsListener(destination="my-destination")
+	@JmsListener(destination = "my-destination")
 	public void receiveMessage(String message) {
 		System.out.println("接收到:<" + message + ">");
 	}
