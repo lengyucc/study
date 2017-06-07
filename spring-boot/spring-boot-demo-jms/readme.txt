bd02f353a7bff0f20822610b0d2513616b5034cd
lengyucc <1902328305@qq.com>
2017-06-07 08:59:42 +0800

    test

---
+++ spring-boot/spring-boot-demo-jms/readme.txt
@@ -0,0 +1,2 @@
+activemq下载地址:http://mirror.bit.edu.cn/apache/activemq/5.14.5/apache-activemq-5.14.5-bin.zip
+activemq启动命令:bin/activemq start
---
+++ spring-boot/spring-boot-demo-jms/src/main/java/com/antbean/springbootdemojms/SpringBootDemoJmsApplication.java
@@ -0,0 +1,24 @@
+package com.antbean.springbootdemojms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.core.JmsTemplate;
+
+@SpringBootApplication
+public class SpringBootDemoJmsApplication implements CommandLineRunner {
+
+	@Autowired
+	JmsTemplate jmsTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoJmsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		jmsTemplate.send("my-destination", new Msg());
+	}
+
+}
