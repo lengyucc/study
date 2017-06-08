22b56ca3eea5bbd5d0b215f90a8fc6d21035cb97
lengyucc <1902328305@qq.com>
2017-06-08 10:18:51 +0800

    没演示成功

---
+++ spring-boot/spring-boot-demo-actuator-http/src/main/java/com/antbean/springbootdemoactuatorhttp/SpringBootDemoActuatorHttpApplication.java
@@ -0,0 +1,12 @@
+package com.antbean.springbootdemoactuatorhttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoActuatorHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoActuatorHttpApplication.class, args);
+	}
+}
